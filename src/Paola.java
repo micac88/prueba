899e4f01dc081e74198c4a589244bc5bfe7aa0e1
Ejemplo <https://github.com/paolaChiappetta/Ejemplo.git>
2021-05-23 19:59:25 -0300

    Agrego clase Paola

---
+++ src/Paola.java
@@ -0,0 +1,20 @@
+public class Paola {
+    public int edad;
+    public String apellido;
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+}
