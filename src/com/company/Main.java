a746b4332e2e0298b5bc4f1e0a165ccb3422f57d
Ejemplo <https://github.com/paolaChiappetta/Ejemplo.git>
2021-05-23 19:55:03 -0300

    Saludo a Mica

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,6 +5,8 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("Hola Mundo");
+
+        System.out.println("Hola Mica");
 	// write your code here
     }
 }
